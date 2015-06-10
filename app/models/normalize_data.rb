843b6b39601c22bfb8145acc87911f0889c2d5a3
Dustin Zeisler <dustin@zeisler.net>
2015-06-09 17:28:26 -0700

    deal with parsing data from hash into models

---
+++ app/models/normalize_data.rb
@@ -0,0 +1,140 @@
+
+#Code could of been sorter, but I wanted to show some OOP. I can think of cases where if the data had lots of error
+#handling and parsing that it could be a nice way to handle changes and isolate complexity.
+class NormalizeData
+
+  def initialize(data:)
+    @data = data
+  end
+
+  def call
+    person
+    merchant
+    product
+    purchase
+    self
+  end
+
+  def person
+    @person ||= Person.find_or_create_by(PersonAttr.new(data).to_h)
+  end
+
+  def merchant
+    @merchant ||= Merchant.find_or_create_by(MerchantAttr.new(data).to_h)
+  end
+
+  def product
+    @product ||= Product.find_or_create_by(ProductAttr.new(data, merchant_id: merchant.id).to_h)
+  end
+
+  def purchase
+    @purchase ||= Purchase.create(PurchaseAttr.new(data, person_id: person.id, product_id: product.id).to_h)
+  end
+
+  private
+
+  class Attr
+
+    def initialize(data, **args)
+      @data         = data
+      @foreign_keys = foreign_keys
+      post_initialize(args)
+    end
+
+    def to_h
+      raise 'Unimplemented #to_h'
+    end
+
+    private
+
+    def post_initialize(*)
+    end
+
+    attr_reader :data, :foreign_keys
+
+  end
+
+  class PersonAttr < Attr
+
+    def to_h
+      { first_name: first_name, last_name: last_name }
+    end
+
+    private
+
+    def first_name
+      data.fetch("purchaser name").split(' ').first
+    end
+
+    def last_name
+      data.fetch("purchaser name").split(' ').last
+    end
+
+  end
+
+  class MerchantAttr < Attr
+
+    def to_h
+      { address: address, name: name }
+    end
+
+    private
+
+    def name
+      data.fetch("merchant name")
+    end
+
+    def address
+      data.fetch("merchant address")
+    end
+
+  end
+
+  class ProductAttr < Attr
+
+    def to_h
+      { description: description, price: price, merchant_id: merchant_id }
+    end
+
+    private
+
+    def post_initialize(merchant_id:)
+      @merchant_id = merchant_id
+    end
+
+    attr_reader :merchant_id
+
+    def description
+      data.fetch("item description")
+    end
+
+    def price
+      data.fetch("item price")
+    end
+
+  end
+
+  class PurchaseAttr < Attr
+
+    def to_h
+      { count: count, person_id: person_id, product_id: product_id }
+    end
+
+    private
+
+    def post_initialize(person_id:, product_id:)
+      @person_id  = person_id
+      @product_id = product_id
+    end
+
+    attr_reader :person_id, :product_id
+
+    def count
+      data.fetch("purchase count")
+    end
+
+  end
+
+  attr_reader :data
+
+end
---
+++ spec/normalize_data_spec.rb
@@ -0,0 +1,93 @@
+require 'spec_helper'
+require 'active_mocker/rspec_helper'
+require_relative '../app/models/normalize_data'
+require 'mocks/merchant_mock'
+require 'mocks/person_mock'
+require 'mocks/purchase_mock'
+require 'mocks/product_mock'
+
+RSpec.describe NormalizeData, active_mocker: true do
+
+  before do
+    ActiveMocker::LoadedMocks.delete_all
+  end
+
+  let(:denormal_data) {
+    { "purchaser name"   => "Snake Plissken",
+      "item description" => "$10 off $20 of food",
+      "item price"       => 10.0,
+      "purchase count"   => 2,
+      "merchant address" => "987 Fake St",
+      "merchant name"    => "Bob's Pizza" }
+  }
+
+  context 'will normalize data into correct models' do
+
+    subject! { NormalizeData.new(data: denormal_data).call }
+
+    it 'person' do
+      expect(Person.first.attributes.except("id")).to eq({ "first_name" => "Snake", "last_name" => "Plissken" })
+    end
+
+    it 'product' do
+      expect(Product.first.description).to eq("$10 off $20 of food")
+      expect(Product.first.price.to_f).to eq(10.0)
+      expect(Product.first.merchant_id).to eq(Merchant.first.id)
+    end
+
+    it 'merchant' do
+      expect(Merchant.first.address).to eq("987 Fake St")
+      expect(Merchant.first.name).to eq("Bob's Pizza")
+    end
+
+    it 'purchase' do
+      expect(Purchase.first.product_id).to eq(Product.first.id)
+      expect(Purchase.first.person_id).to eq(Person.first.id)
+      expect(Purchase.first.count).to eq(2)
+    end
+
+  end
+
+  context 'reference data will only be created once' do
+    let(:denormal_data1) {
+      { "purchaser name"   => "Snake Plissken",
+        "item description" => "$10 off $20 of food",
+        "item price"       => 10.0,
+        "purchase count"   => 1,
+        "merchant address" => "987 Fake St",
+        "merchant name"    => "Bob's Pizza" }
+    }
+    let(:denormal_data2) {
+      { "purchaser name"   => "Snake Plissken",
+        "item description" => "$10 off $20 of food",
+        "item price"       => 10.0,
+        "purchase count"   => 2,
+        "merchant address" => "987 Fake St",
+        "merchant name"    => "Bob's Pizza" }
+    }
+
+    before do
+      NormalizeData.new(data: denormal_data1).call
+      NormalizeData.new(data: denormal_data2).call
+    end
+
+    it 'purchase is created for every time' do
+      expect(Purchase.count).to eq 2
+      expect(Purchase.all.map(&:count)).to eq [1,2]
+    end
+
+    it 'merchant' do
+      expect(Merchant.count).to eq 1
+    end
+
+    it 'person' do
+      expect(Person.count).to eq 1
+    end
+
+    it 'product' do
+      expect(Product.count).to eq 1
+    end
+
+  end
+
+end
